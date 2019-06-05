c4f82b1993a43682890594f16101af48d121a50d
ryumi <120808854@qq.com>
2019-06-05 16:45:15 +0800

    弹窗

---
+++ Console/js/alert.js
@@ -0,0 +1,11 @@
+function one(){
+    alert("I have a dream !\nI want to play games");
+    alert("Oh! This game is so wonderful!");
+    alert("I like it.");
+    alert("Can you play with me?");
+    alert("Really?");
+    alert("Enjoy the game!");
+    alert("Haha!");
+    alert("---Game over---");
+}
+one();
